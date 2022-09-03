e9c8c2f59db40c450b274065fee2108dcf37d3a1
Brian Chen <bzchen93@gmail.com>
2022-09-03 13:52:06 +1000

    migrate cli to use clap

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use serde_json::from_str;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use colored::Colorize;
+use clap::Parser;
 
 const CONFIG_FILE_NAME: &str = "particle.config.json";
 
@@ -57,7 +58,7 @@ fn get_config() -> Result<(ParticleConfig, String), String> {
     }
 }
 
-fn install(config: &ParticleConfig, root_path: &String) {
+fn check(config: &ParticleConfig, root_path: &String) {
     println!("install deps I guess");
     // Pull workspace data
     println!("Workspaces are {:?}", config.workspaces);
@@ -90,17 +91,33 @@ fn install(config: &ParticleConfig, root_path: &String) {
     // only write new lockfile now after successful install
 }
 
+/// An unopinionated monorepo package manager for JS based applications.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+   /// Check https://github.com/Brianzchen/particle#usage to learn more about available commands
+   command: String,
+
+   /// When running the `workspace` command, the package you'd like to target
+   arg_2: Option<String>,
+}
+
 fn main() {
     let (config, root_path) = get_config()
         .expect("`particle.config.json` not found. You should add one to the root of your project to get started");
 
-    let args: Vec<String> = env::args().collect();
-    let query = if args.len() > 1 { &args[1] } else { "" };
+    let args = Args::parse();
+    // let query = if args.len() > 1 { &args[1] } else { "" };
+    let command = &args.command;
 
-    if query == "install" {
-        install(&config, &root_path);
-    } else if query == "help" {
-        println!("give some helpful hints full of commands")
+    if command == "check" {
+        check(&config, &root_path);
+    } else if command == "workspace" {
+        println!("pull the package name");
+        if args.arg_2 == None {
+            println!("You've called `workspace` without the --package option");
+        }
+        println!("{:?}", args.arg_2);
     } else {
         println!("{}, try `{}` for more information",
             format!("Invalid command given").red().bold(),
